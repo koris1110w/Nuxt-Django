be345b39e55e7a71b5f1c59afe55d7f75eb54b59
koris1110w <koris1110w@gmail.com>
2024-05-18 18:28:08 +0900

    test

---
+++ frontend/pages/sample/index.vue
@@ -0,0 +1,18 @@
+<template>
+  test
+  <el-button
+    round
+    type="primary"
+    size="large"
+    class="text-sm font-semibold mt-4 w-40"
+    @click="aaa()"
+  >button</el-button>
+</template>
+<script setup>
+  const aaa = async () => {
+    console.log("test")
+    const runtimeConfig = useRuntimeConfig();
+    const { data: riddles } = await useFetch(`${runtimeConfig.public.apiUrl}/api/v1/riddles/`)
+    console.log(riddles.value.results[0].name)
+  }
+</script>
