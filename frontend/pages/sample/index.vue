b876570f7320023c34925f20e42b205e697d0675
koris1110w <koris1110w@gmail.com>
2024-05-24 12:52:38 +0900

    test

--- frontend/pages/sample/index.vue
+++ frontend/pages/sample/index.vue
@@ -12,14 +12,12 @@
   </ClientOnly>
 </template>
 <script setup>
-  onMounted( async () => {
-    const runtimeConfig = useRuntimeConfig();
-    console.log(runtimeConfig.public.test)
-    console.log(runtimeConfig.public.apiUrl)
-    const { data: samples, error } = await useFetch(`${runtimeConfig.public.apiUrl}/api/v1/riddles/`)
-    console.log("samples")
-    console.log(samples)
-  })
+  const runtimeConfig = useRuntimeConfig();
+  console.log(runtimeConfig.public.test)
+  console.log(runtimeConfig.public.apiUrl)
+  const { data: samples, error } = await useFetch(`${runtimeConfig.public.apiUrl}/api/v1/riddles/`)
+  console.log("samples")
+  console.log(samples)
   const aaa = async () => {
     console.log("test")
     const runtimeConfig = useRuntimeConfig();
